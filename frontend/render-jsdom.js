0b58864d3371219ab6d8b60a47ab33cf876fa4c1
Ariya Hidayat <ariya@metabase.com>
2021-05-28 12:24:58 -0700

    Load bundles with JSDOM

--- frontend/render-jsdom.js
+++ frontend/render-jsdom.js
@@ -1,11 +1,30 @@
 const fs = require("fs");
-const { Script } = require("vm");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const HTMLContent = `<!DOCTYPE html></html>`;
+const HTMLContent = `<!DOCTYPE html>
+<head>
+<script src="http://app/dist/vendor.bundle.js"></script>
+<script src="http://app/dist/styles.bundle.js"></script>
+<script src="http://app/dist/vizrender.bundle.js"></script>
+</head>
+<body>
+<script>console.log('Bootstrapping with JSDOM')</script>
+</body>
+</html>`;
 
-const dom = new JSDOM(HTMLContent, { runScripts: "dangerously" });
-const vmContext = dom.getInternalVMContext();
-
-new Script("console.log('Bootstrapping with JSDOM')").runInContext(vmContext);
+class CustomResourceLoader extends jsdom.ResourceLoader {
+  fetch(url, options) {
+    const fname = "resources/frontend_client/" + url.replace("http://", "");
+    const content = fs.readFileSync(fname);
+    console.log(`Loading ${fname} -> ${content.length} bytes`);
+    return Promise.resolve(content);
+  }
+}
+const resources = new CustomResourceLoader();
+const runScripts = "dangerously";
+const dom = new JSDOM(HTMLContent, {
+  resources,
+  runScripts,
+  storageQuota: 10000000,
+});
